a0431a5d2d5ed997d3299a45d34e9dd9500a20f0
rao-suraj <surajrao250@gmail.com>
2025-03-03 19:54:50 +0530

    human_tool_and_prompt_update

--- src/tasklist_agent_crew_ai/tools/human_interacton_tool.py
+++ src/tasklist_agent_crew_ai/tools/human_interacton_tool.py
@@ -4,11 +4,11 @@ from pydantic import BaseModel, Field
 
 class HumanInteractionInput(BaseModel):
     """Input schema for HumanInteractionTool."""
-    argument: str = Field(..., description="The question that you want to ask for the human.")
+    argument: str = Field(..., description="The question that you want to ask for the human or documents for which you want to get approval from the human.")
 
 class HumanInteractionTool(BaseTool):
     name: str = "Human Interaction Tool"
-    description: str = "This tool will help you to ask questions to the user. I task questions as String and return human answers as String."
+    description: str = "This tool will help you to ask questions to the human or ask for approval for the documents .Send input as String and return human answers as String."
     args_schema: Type[BaseModel] = HumanInteractionInput
 
     def _run(self, argument: str) -> str:
